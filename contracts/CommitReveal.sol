d0a817ea0936daebd01b2af13c4ac625c3d1f4c9
lazykern <phusitsom@gmail.com>
2024-03-25 17:48:48 +0700

    Rename CommitReveal parameter

--- contracts/CommitReveal.sol
+++ contracts/CommitReveal.sol
@@ -10,38 +10,38 @@ contract CommitReveal {
 
     mapping(uint256 => Commit) internal commits;
 
-    function commit(uint256 lotteryId, bytes32 dataHash) internal {
-        commits[lotteryId].commit = dataHash;
-        commits[lotteryId].block = uint64(block.number);
-        commits[lotteryId].revealed = false;
+    function commit(uint256 index, bytes32 dataHash) internal {
+        commits[index].commit = dataHash;
+        commits[index].block = uint64(block.number);
+        commits[index].revealed = false;
         emit CommitHash(
             msg.sender,
-            lotteryId,
-            commits[lotteryId].commit,
-            commits[lotteryId].block
+            index,
+            commits[index].commit,
+            commits[index].block
         );
     }
     event CommitHash(
         address sender,
-        uint256 lotteryId,
+        uint256 index,
         bytes32 dataHash,
         uint64 block
     );
 
     function revealChoice(
-        uint256 lotteryId,
+        uint256 index,
         uint16 choice,
         string memory salt
     ) internal {
         //make sure it hasn't been revealed yet and set it to revealed
         require(
-            commits[lotteryId].revealed == false,
+            commits[index].revealed == false,
             "CommitReveal::revealChoice: Already revealed"
         );
-        commits[lotteryId].revealed = true;
+        commits[index].revealed = true;
         //require that they can produce the committed hash
         require(
-            getSaltedHash(choice, salt) == commits[lotteryId].commit,
+            getSaltedHash(choice, salt) == commits[index].commit,
             "CommitReveal::revealChoice: Revealed hash does not match commit"
         );
         emit RevealChoice(msg.sender, choice, salt);
